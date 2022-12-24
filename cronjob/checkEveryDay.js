507933e1c1c8cabd730e66cd9a32274085bd3b53
natreeum <projectkmscom@gmail.com>
2022-12-24 19:09:00 +0900

    fix : time standard change from UTC to KST

--- cronjob/checkEveryDay.js
+++ cronjob/checkEveryDay.js
@@ -2,18 +2,20 @@ const { resetDailyData } = require(`../prismaScripts/dailycheck`);
 
 const checkIsNewDay = async () => {
   const now = new Date();
-  const hour = now.getHours();
-  const minute = now.getMinutes();
+  const time_diff = 9 * 60 * 60 * 1000;
+  const kst = new Date(now.getTime() + time_diff);
+  const hour = kst.getHours();
+  const minute = kst.getMinutes();
   const time = '' + hour + minute;
 
-  const year = now.getFullYear();
-  const month = now.getMonth() + 1;
-  const date = now.getDate();
+  const year = kst.getFullYear();
+  const month = kst.getMonth() + 1;
+  const date = kst.getDate();
 
   if (time === '00') {
     await resetDailyData();
     console.log(
-      `[UTC : ${
+      `[KST: ${
         '' + year + month + date + time
       }] Daily Mining data has been reset`
     );
